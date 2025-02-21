ec9fa746a7539b7a6a82c82e39690ba98f2ed613
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-02-21 14:52:50 +0200

    corrects route

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -8,13 +8,13 @@ from models.city import City
 from models.state import State
 
 
-@app_views.route("/api/v1/states/<state_id>/cities", methods=['GET'])
+@app_views.route("/states/<state_id>/cities", methods=['GET'], strict_slashes=False)
 def get_cities_of_state(state_id):
     """return cities of a state"""
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
-    cities = []
-    for city in state.cities:
-        cities.append(city.to_dict())
-    return jsonify(cities)
+    cities = state.cities
+    return make_response(jsonify([city.to_dict() for city in cities]))
+
+
