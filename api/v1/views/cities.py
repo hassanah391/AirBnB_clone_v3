0bc2136ec0866348c53f07debaa04e966b1dee74
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-02-21 13:51:11 +0200

    handles retriving of cities of a state

---
+++ api/v1/views/cities.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""cities.py"""
+
+from api.v1.views import app_views
+from flask import abort, jsonify, make_response, request
+from models import storage
+from models.city import City
+from models.state import State
+
+
+@app_views.route("/api/v1/states/<state_id>/cities", methods=['GET'])
+def get_cities_of_state(state_id):
+    """return cities of a state"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    cities = []
+    for city in state.cities:
+        cities.append(city.to_dict())
+    return jsonify(cities)
